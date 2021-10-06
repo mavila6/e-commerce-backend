a6bceccdf002cc47c156a9e454473cdee66572fe
Marco Avila <mavila6@email.arizona.com>
2021-10-06 07:14:46 -0700

    fixed error in the belong to many tag

--- models/index.js
+++ models/index.js
@@ -15,7 +15,7 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'product_tags',
+  as: 'products',
   foreignKey: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
